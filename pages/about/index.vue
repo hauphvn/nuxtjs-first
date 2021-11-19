8d3ae681ec2366e3e9844d8d22e27de0ea0f3e70
hauphvn <pvhau@cmc.com.vn>
2021-11-19 15:18:31 +0700

    Creating pages related

---
+++ pages/about/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="about-page">
+  <h1>This is a about page</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias ea non odio pariatur quaerat sapiente suscipit totam unde voluptate? Aliquam amet assumenda deleniti earum eveniet fugit temporibus, unde voluptatem.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias ea non odio pariatur quaerat sapiente suscipit totam unde voluptate? Aliquam amet assumenda deleniti earum eveniet fugit temporibus, unde voluptatem.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias ea non odio pariatur quaerat sapiente suscipit totam unde voluptate? Aliquam amet assumenda deleniti earum eveniet fugit temporibus, unde voluptatem.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias ea non odio pariatur quaerat sapiente suscipit totam unde voluptate? Aliquam amet assumenda deleniti earum eveniet fugit temporibus, unde voluptatem.</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
