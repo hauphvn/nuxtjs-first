e536168462a8c05e88841913b8ee6f5a5175167d
hauphvn <pvhau@cmc.com.vn>
2021-11-19 10:23:53 +0700

    -Subrouting; test link on markdown

---
+++ pages/user/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <h1>This is a single user with ID: {{$route.params.id}}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
