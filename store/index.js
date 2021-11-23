d9bc21478e46281ff37d66891f6dcb185dba407f
hauphvn <pvhau@cmc.com.vn>
2021-11-23 11:26:12 +0700

    Modifing clear token

--- store/index.js
+++ store/index.js
@@ -19,6 +19,9 @@ const createStore = () => {
       },
       setToken (state, token) {
         state.token = token
+      },
+      clearToken (state) {
+        state.token = null
       }
     },
     actions: {
@@ -91,6 +94,7 @@ const createStore = () => {
         })
           .then((resp) => {
             vuexContext.commit('setToken', resp.idToken)
+            vuexContext.dispatch('setLogoutTimer', 1000 * 60 * 15)
             // {
             //   "kind": "identitytoolkit#SignupNewUserResponse",
             //   "idToken": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjJlMzZhMWNiZDBiMjE2NjYxOTViZGIxZGZhMDFiNGNkYjAwNzg3OWQiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vbnV4dC1ibG9nLWZpcnN0IiwiYXVkIjoibnV4dC1ibG9nLWZpcnN0IiwiYXV0aF90aW1lIjoxNjM3NjM2MjI1LCJ1c2VyX2lkIjoiVjlwZnFUdjRCWVJtZWZrSEdURnI4N3ZpMlpIMyIsInN1YiI6IlY5cGZxVHY0QllSbWVma0hHVEZyODd2aTJaSDMiLCJpYXQiOjE2Mzc2MzYyMjUsImV4cCI6MTYzNzYzOTgyNSwiZW1haWwiOiJoYXVwaHZuQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJoYXVwaHZuQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6InBhc3N3b3JkIn19.JFk9f-MoOiMtZuHAGBBv7Op4QlRttrW2JjeMnvyG39G-3CJoy9zU_-81e47p8Qi2Mb-fuA5f_iKVXU6Ss8ZLZwRxHXQEPzRcAZB7nLpm0kv8xWIQ1whGcBqax0pr1H5RygmfJJB1CdgOuuOlZ14sprlDLME8bwbZeTUnAErgK64Xq8VkfEX-Zg3Issy3q8ALmVQzxH3Nae4cK4ADV_PaQ3sQKBvEzs9ah_LUyfiWZua-P5J1214svCtai4sye3BAUonUIqM3eDDcUae_D1aDr0gpPl2AB9aiHtzmPTakFN0g-8P7WG_jZB9sw6Re7vp3SYTVNx9lrxQ06fWpDS9yMg",
@@ -104,7 +108,13 @@ const createStore = () => {
             this.alertContent = 'Email existed'
             this.isAlertEmail = true
           })
+      },
+      setLogoutTimer (vuexContext, duration) {
+        setTimeout(() => {
+          vuexContext.commit('clearToken')
+        }, duration)
       }
+
     },
     getters: {
       loadedPosts (state) {
